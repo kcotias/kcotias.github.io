fece9bc4008be1b657f488cf9683d00937b1b199
kcotias <kaianexperia@gmail.com>
2019-04-23 16:10:53 -0300

    buildon

---
+++ precache-manifest.3564cbc896a3e95e5207b7552a1f655b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19e0be735d8e01c20084",
+    "url": "/static/css/main.cbfcc472.chunk.css"
+  },
+  {
+    "revision": "19e0be735d8e01c20084",
+    "url": "/static/js/main.19e0be73.chunk.js"
+  },
+  {
+    "revision": "8dbfb0a2d1486aa2927a",
+    "url": "/static/js/1.8dbfb0a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b54d031aa3d79cdfb27dbd43cd803ef8",
+    "url": "/index.html"
+  }
+];
